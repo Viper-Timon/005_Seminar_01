b7b5b2718e4823f89161e23e0a3cb6201075a8b7
viper-timon <viper-timon@mail.ru>
2022-04-25 20:56:43 +0300

    final task_0

---
+++ Tast_0/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите прог., которая на вход принимает число
+// и выдает его квадрат (само на себя)
+Console.Write("Введите число, чтобы возвести в квадрат -> ");
+int num = int.Parse(Console.ReadLine());
+int numResult = num * num;
+Console.Write("Квадрат числа равен = ");
+Console.WriteLine(numResult);
+Console.ReadKey();
