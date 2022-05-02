6ed256cd9cf819c1432e7835b86f9a5a3e4dbcf4
viper-timon <viper-timon@mail.ru>
2022-05-02 16:51:05 +0300

    HW task 4 done

---
+++ HomeWork/HWT_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// ДЗ с семинара №1
+// Задача 4: Напишите программу,вход 3 числа
+// Выдает максимальное из 3х
+Console.Write("Введите первое число -> ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число -> ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число -> ");
+int num3 = int.Parse(Console.ReadLine());
+int max = num1;
+if (max < num2) max = num2; 
+if (max < num3) max = num3; 
+Console.Write("Максимальное число из трех введенных -> ");
+Console.WriteLine(max);
