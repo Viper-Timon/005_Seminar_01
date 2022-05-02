8457306e1e9eb8d542882bdaa1fd6c7f3c66b88d
viper-timon <viper-timon@mail.ru>
2022-05-02 16:10:32 +0300

    fixed task 5 for -

--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -1,6 +1,7 @@
-﻿Console.Write("Введите число -> ");
+﻿Console.Write("Введите число -> "); // программа с вводом числа и выведением от -1*число до числа всех значений
 int n = int.Parse(Console.ReadLine());
 int i;
+if (n<0) n = n * -1;
 for (i = n*-1; i <= n; i++) 
 {
     Console.Write(i);
