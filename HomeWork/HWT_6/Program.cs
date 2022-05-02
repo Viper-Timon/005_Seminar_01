01bf55b5a70c5ed1e7493c10bfe39bc23643c32a
viper-timon <viper-timon@mail.ru>
2022-05-02 17:03:06 +0300

    HW task 6 done

---
+++ HomeWork/HWT_6/Program.cs
@@ -0,0 +1,18 @@
+﻿// ДЗ с семинара №1
+// Задача 6: Напишите программу,вход 1 число
+// Выдает четное ли число
+Console.Write("Введите число -> ");
+int num1 = int.Parse(Console.ReadLine());
+int res = num1 % 2;
+if (res == 0)
+{
+    Console.Write("Введенное число -> ");
+    Console.Write(num1);
+    Console.WriteLine(" чётное");
+}
+else
+{
+    Console.Write("Введенное число -> ");
+    Console.Write(num1);
+    Console.WriteLine(" нечётное");   
+}
